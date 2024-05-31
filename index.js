45bef80c1c6d1b9345811bd2ba7f92ea044fa43c
abhishekn1208 <abhisheknigam735@gmail.com>
2024-05-31 14:29:19 +0530

    uploaded

---
+++ index.js
@@ -0,0 +1,90 @@
+const ladiesDiv = document.getElementsByClassName("ladies");
+const flex_parent = document.getElementsByClassName("div-flex-parent");
+const menDiv = document.getElementsByClassName("men");
+const flex_parent2 = document.getElementsByClassName("div2-flex-parent");
+const babyDiv = document.getElementsByClassName('baby');
+const flex_parent3 = document.getElementsByClassName('div3-flex-parent')
+const kidsDiv = document.getElementsByClassName('kids');
+const flex_parent4 = document.getElementsByClassName('div4-flex-parent')
+const homeDiv = document.getElementsByClassName('home');
+const flex_parent5 = document.getElementsByClassName('div5-flex-parent')
+const sportsDiv = document.getElementsByClassName('sport');
+const flex_parent6 = document.getElementsByClassName('div6-flex-parent');
+const saleDiv = document.getElementsByClassName('sale');
+const flex_parent7 = document.getElementsByClassName('div7-flex-parent')
+const sustainabilityDiv = document.getElementsByClassName('sustainability');
+const flex_parent8 = document.getElementsByClassName('div8-flex-parent')
+
+
+ladiesDiv[0].addEventListener('mouseover', () => {
+    flex_parent[0].style.display='flex';
+})
+flex_parent[0].addEventListener('mouseover', () => {
+    flex_parent[0].style.display='flex';
+})
+flex_parent[0].addEventListener('mouseout', () => {
+    flex_parent[0].style.display='none';
+})
+menDiv[0].addEventListener('mouseover', ()=>{
+    flex_parent2[0].style.display = 'flex';
+})
+flex_parent2[0].addEventListener('mouseover', ()=>{
+    flex_parent2[0].style.display = 'flex';
+})
+flex_parent2[0].addEventListener('mouseout',()=>{
+    flex_parent2[0].style.display = 'none';
+})
+babyDiv[0].addEventListener('mouseover', ()=>{
+    flex_parent3[0].style.display='flex'
+})
+flex_parent3[0].addEventListener('mouseover', ()=>{
+    flex_parent3[0].style.display='flex'
+})
+flex_parent3[0].addEventListener('mouseout', ()=>{
+    flex_parent3[0].style.display ='none'
+})
+kidsDiv[0].addEventListener('mouseover', ()=>{
+    flex_parent4[0].style.display = 'flex'
+})
+flex_parent4[0].addEventListener('mouseover', ()=>{
+    flex_parent4[0].style.display = 'flex';
+})
+flex_parent4[0].addEventListener('mouseout', ()=>{
+    flex_parent4[0].style.display = 'none'
+})
+homeDiv[0].addEventListener('mouseover', ()=>{
+    flex_parent5[0].style.display='flex';
+})
+flex_parent5[0].addEventListener('mouseover', ()=>{
+    flex_parent5[0].style.display = 'flex'
+})
+flex_parent5[0].addEventListener('mouseout', ()=>{
+    flex_parent5[0].style.display = 'none'
+})
+sportsDiv[0].addEventListener('mouseover', ()=>{
+    flex_parent6[0].style.display = 'flex'
+})
+flex_parent6[0].addEventListener('mouseover', ()=>{
+    flex_parent6[0].style.display = 'flex'
+})
+flex_parent6[0].addEventListener('mouseout', ()=>{
+    flex_parent6[0].style.display = 'none'
+})
+saleDiv[0].addEventListener('mouseover', ()=>{
+    flex_parent7[0].style.display = 'flex'
+})
+flex_parent7[0].addEventListener('mouseover', ()=>{
+    flex_parent7[0].style.display = 'flex';
+})
+flex_parent7[0].addEventListener('mouseout',()=>{
+    flex_parent7[0].style.display = 'none'
+})
+sustainabilityDiv[0].addEventListener('mouseover', ()=>{
+    flex_parent8[0].style.display='flex'
+})
+flex_parent8[0].addEventListener('mouseover', ()=>{
+    flex_parent8[0].style.display = 'flex'
+})
+flex_parent8[0].addEventListener('mouseout', ()=>{
+    flex_parent8[0].style.display = 'none'
+})
